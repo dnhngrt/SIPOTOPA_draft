caa14e24e9bdb98e25cf87e880cafaeed8ab6563
Eka Juliantara <juliantaraeka@gmail.com>
2020-07-10 18:27:53 +0700

    Update the admin and add a new user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,10 +14,22 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'eka',
-            'email' => 'eka@sipotopa.localhost',
-            'password' => Hash::make('eka123'),
+            'name' => 'admin',
+            'email' => 'admin@sipotopa.localhost',
+            'password' => Hash::make('admin123'),
             'admin' => 1
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'eka',
+            'email' => 'eka@email.com',
+            'password' => Hash::make('eka123'),
+            'address' => 'Jl. Anggur No. 5',
+            'city' => 'Sleman',
+            'state' => 'D. I. Yogyakarta',
+            'country' => 'Indonesia',
+            'pincode' => '50000',
+            'mobile' => '081081081081'
         ]);
     }
 }
